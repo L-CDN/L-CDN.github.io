5a517fb13dfe0ee66a982e9441fd567aa9f44b98
L-CDN <liamlanders72@hotmail.ca>
2025-01-22 09:42:00 -0500

    Initial commit

---
+++ website/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+  page='home'
+  return render(request, 'index.html')
+
+def about(request):
+  page='about'
+  return render(request, 'about.html')
+
+def contact(request):
+  page='contact'
+  return render(request, 'contact.html')
+
+def projects(request):
+  page='projects'
+  return render(request, 'projects.html')
+
+def styles(request):
+  page='styles'
+  return render(request, 'styles.html')
